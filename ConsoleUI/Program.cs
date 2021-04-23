4485f3e759145e0b1dbe6d602942bf532a6b78b1
mislinaaydn <mislinaaydn26305@gmail.com>
2021-04-24 01:21:15 +0300

    BTK kurs

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,20 +1,37 @@
 ﻿
 using DataAccess.Concrete.EntityFramework;
-
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
 {
-    
-    class Program
+
+     class Program
     {
         static void Main(string[] args)
         {
+            EfProductDal efProductDal = new EfProductDal();
             
+            Product product1 = new Product
+            {
+                CategoryId = 1,
+                BrandId = 1,
+                Name = "su",
+                Price = 2,
+                CreateDate = DateTime.UtcNow,
+                Code = "WTR01",
+                Active = true
+            };
+            efProductDal.Add(product1);
+
+            foreach (var product in efProductDal.GetAll())
+            {
+                Console.WriteLine(product.Name);
+            }
         }
 
-        
 
-       
+
+
     }
 }
